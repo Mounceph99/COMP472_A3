f3ce7cd2bc99d24f388c40c0aea36e3adcdc30e7
Luc Nguyen <1999lucnguyen@gmail.com>
2021-04-08 00:51:58 -0400

    Implement minimax with alpha-beta pruning, along with statistics

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 import sys
 
+# Class that stores the statistics of the alpha-beta pruning algorithm ran
+class Statistics:
+    def __init__(self):
+        self.__num_nodes_visited = 0
+        self.__num_nodes_evaluated = 0 # (either an end game state or the specified depth is reached)
+        self.__max_depth_reached = 0
+
+        # used to calculate average effective branching factor (i.e., the average number of successors that are not pruned)
+        # it will be equal to sum(branching_factors) / len(branching_factors)
+        self.__branching_factors = []
+    
+    def get_statistics(self):
+        """
+        Returns number of nodes visited, number of nodes evaluated, max depth reached, and average effective branching factor
+        of a particular execution of minimax with alpha-beta pruning.
+        """
+
+        avg_branching_factor = 0 if len(self.__branching_factors) == 0 else sum(self.__branching_factors)/len(self.__branching_factors)
+
+        return self.__num_nodes_visited, self.__num_nodes_evaluated, self.__max_depth_reached, avg_branching_factor
+    
+    def increment_num_nodes_visited(self):
+        """
+        Increments number of nodes visited.
+        """
+
+        self.__num_nodes_visited += 1
+    
+    def increment_num_nodes_evaluated(self):
+        """
+        Increments number of nodes evaluated.
+        """
+
+        self.__num_nodes_evaluated += 1
+    
+    def set_max_depth_reached(self, depth):
+        """
+        Sets max_depth_reached to passed depth if it is larger.
+        """
+
+        self.__max_depth_reached = max(self.__max_depth_reached, depth)
+    
+    def add_branching_factor(self, branching_factor):
+        """
+        Add branching factor to list of branching factors.
+        """
+
+        self.__branching_factors.append(branching_factor)
+
+
+
 def get_possible_moves(n_tokens, n_taken_tokens, taken_tokens, last_move):
     """
     Get possible moves according to current state of the game.
@@ -39,6 +90,10 @@ def get_possible_moves(n_tokens, n_taken_tokens, taken_tokens, last_move):
     return possible_moves
 
 def is_prime(num):
+    """
+    Returns true if passed num is prime.
+    """
+
     if num <= 1:
         # 1 is not prime
         return False
@@ -61,7 +116,7 @@ def is_prime(num):
 
         return True
 
-def heuristic(last_move, n_taken_tokens, taken_tokens, next_possible_moves, is_max_player_turn):
+def heuristic(last_move, taken_tokens, next_possible_moves, is_max_player_turn):
     """
     Evaluate the state of the game.
     """
@@ -114,6 +169,110 @@ def heuristic(last_move, n_taken_tokens, taken_tokens, next_possible_moves, is_m
     # return result. negate result if it is not max player's turn
     return result if is_max_player_turn else -result
 
+
+def alpha_beta_search(n_tokens, n_taken_tokens, taken_tokens, last_move, max_depth):
+    """
+    Using minimax with alpha-beta pruning, return, according to state of the game:
+    - The best move (i.e., the tokens number that is to be taken) for the current player
+    - The value associated with the move
+    - The total number of nodes visited
+    - The number of nodes evaluated (either an end game state or the specified depth is reached)
+    - The maximum depth reached (the root is at depth 0)
+    - The average effective branching factor (i.e., the average number of successors that are not pruned)
+    """
+
+    best_move_value, best_move = None, None
+
+    # instantiate Statistics object that will store algorithm's execution statistics
+    statistics = Statistics()
+
+    is_max_player_turn = (n_taken_tokens%2 == 0)
+    if is_max_player_turn:
+        best_move_value, best_move = max_value(n_tokens, n_taken_tokens, taken_tokens, last_move, 0, max_depth, float('-inf'), float('inf'), statistics)
+    else:
+        best_move_value, best_move = min_value(n_tokens, n_taken_tokens, taken_tokens, last_move, 0, max_depth, float('-inf'), float('inf'), statistics)
+
+    return best_move, best_move_value, *statistics.get_statistics()
+
+def max_value(n_tokens, n_taken_tokens, taken_tokens, last_move, depth, max_depth, alpha, beta, statistics):
+    statistics.set_max_depth_reached(depth)
+    statistics.increment_num_nodes_visited()
+
+    next_possible_moves = get_possible_moves(n_tokens, n_taken_tokens, taken_tokens, last_move)
+    if len(next_possible_moves) == 0 or (max_depth != 0 and depth == max_depth):
+        # if terminal state or we reached max_depth
+        statistics.increment_num_nodes_evaluated()
+        return heuristic(last_move, taken_tokens, next_possible_moves, True), None
+    
+    v = float('-inf')
+    best_move = None
+
+    total_branches_visited = 0
+
+    for move in next_possible_moves:
+        # take token
+        taken_tokens[move] = True
+
+        v2, move2 = min_value(n_tokens, n_taken_tokens + 1, taken_tokens, move, depth + 1, max_depth, alpha, beta, statistics)
+
+        total_branches_visited += 1
+
+        if (v2 > v) or (v2 == v and (best_move is None or move < best_move)):
+            # if this is a better move
+            # or if they are equal in value, but the move is a smaller token,
+            # set new v and best_move
+            v, best_move = v2, move
+            alpha = max(alpha, v)
+
+        # backtrack
+        taken_tokens[move] = False
+
+        if v >= beta:
+            break
+
+    statistics.add_branching_factor(total_branches_visited)
+    return v, best_move
+
+
+def min_value(n_tokens, n_taken_tokens, taken_tokens, last_move, depth, max_depth, alpha, beta, statistics):
+    statistics.set_max_depth_reached(depth)
+    statistics.increment_num_nodes_visited()
+
+    next_possible_moves = get_possible_moves(n_tokens, n_taken_tokens, taken_tokens, last_move)
+    if len(next_possible_moves) == 0 or (max_depth != 0 and depth == max_depth):
+        # if terminal state or we reached max_depth
+        statistics.increment_num_nodes_evaluated()
+        return heuristic(last_move, taken_tokens, next_possible_moves, False), None
+    
+    v = float('inf')
+    best_move = None
+
+    total_branches_visited = 0
+
+    for move in next_possible_moves:
+        # take token
+        taken_tokens[move] = True
+
+        v2, move2 = max_value(n_tokens, n_taken_tokens + 1, taken_tokens, move, depth + 1, max_depth, alpha, beta, statistics)
+
+        total_branches_visited += 1
+
+        if (v2 < v) or (v2 == v and (best_move is None or move < best_move)):
+            # if this is a better move
+            # or if they are equal in value, but the move is a smaller token,
+            # set new v and best_move
+            v, best_move = v2, move
+            beta = min(beta, v)
+
+        # backtrack
+        taken_tokens[move] = False
+
+        if v <= alpha:
+            break
+    
+    statistics.add_branching_factor(total_branches_visited)
+    return v, best_move
+
 if __name__ == '__main__':
     ######## Get passed info (#tokens, #taken_tokens, list_of_taken_tokens (and last move), depth) ########
     n_tokens = int(sys.argv[1])
@@ -129,3 +288,15 @@ if __name__ == '__main__':
     depth = int(sys.argv[-1])
 
     last_move = None if n_taken_tokens == 0 else int(sys.argv[-2])
+
+    # Perform minimax with alpha-beta pruning to find best move
+    best_move, best_move_value, num_nodes_visited, num_nodes_evaluated, max_depth_reached, avg_branching_factor \
+        = alpha_beta_search(n_tokens, n_taken_tokens, taken_tokens, last_move, depth)
+    
+    # Print results
+    print('Move: {}'.format(best_move))
+    print('Value: {:.1f}'.format(best_move_value))
+    print('Number of Nodes Visited: {}'.format(num_nodes_visited))
+    print('Number of Nodes Evaluated: {}'.format(num_nodes_evaluated))
+    print('Max Depth Reached: {}'.format(max_depth_reached))
+    print('Avg Effective Branching Factor: {:.1f}'.format(avg_branching_factor))
